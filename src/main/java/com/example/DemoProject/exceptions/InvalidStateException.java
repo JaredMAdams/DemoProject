b107906e53769c0171b0573615e72d11cdd5d074
JaredMAdams <109548288+JaredMAdams@users.noreply.github.com>
2023-06-17 14:02:33 -0500

    Delete src/main/java/com/example/DemoProject directory

--- src/main/java/com/example/DemoProject/exceptions/InvalidStateException.java
+++
@@ -1,8 +0,0 @@
-package com.example.demoproject.exceptions;
-
-public class InvalidStateException extends RuntimeException{
-
-    public InvalidStateException(String message) {
-        super(message);
-    }
-}
